c7bddeb33ebb90a6f0530166c1290dacbda5f405
CHALLA MUKESH REDDY <challamukeshreddy2006@gmail.com>
2026-06-13 15:57:07 +0530

    Add mock seat data

---
+++ src/data/seats.js
@@ -0,0 +1,13 @@
+export const seats = [
+  { id: "A1", status: "free" },
+  { id: "A2", status: "occupied" },
+  { id: "A3", status: "away" },
+
+  { id: "B1", status: "free" },
+  { id: "B2", status: "occupied" },
+  { id: "B3", status: "free" },
+
+  { id: "C1", status: "away" },
+  { id: "C2", status: "free" },
+  { id: "C3", status: "occupied" }
+];
